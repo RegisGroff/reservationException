d66f35e9ef3aae75fa57fdc5a0dcb3b85ed450d8
RegisGroff <regisdsg@hotmail.com>
2023-07-24 21:01:27 -0300

    Primeira solução (Tratamento de excecoes no Program)

---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,56 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+    private Integer roomNumber;
+    private Date checkIn;
+    private Date checkOut;
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    //Static para não ser instanciado um novo sdf para cada objeto reservation que o app tiver
+
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+    public long duration(){
+        long diff = checkOut.getTime() - checkIn.getTime();//Diferença das duas datas em mS
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);//Converte de mS para dias
+    }
+    public void updateDates(Date checkIn, Date checkOut){
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    @Override
+    public String toString() {
+        return "Room "
+                + roomNumber
+                + ", check-in: "
+                + sdf.format(checkIn)
+                + ", check-out: "
+                + sdf.format(checkOut)
+                + ", "
+                + duration()
+                + " nights";
+    }
+}
